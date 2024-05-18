53d90f597e8bb1935f89cea3f4e5fc12118cafcc
Jerry <75060346+ryan2414@users.noreply.github.com>
2024-05-18 23:34:17 +0900

    위젯을 상하좌우로 배치

--- lib/main.dart
+++ lib/main.dart
@@ -7,45 +7,81 @@ void main() {
         title: Text('Study to Container'),
         backgroundColor: Colors.blue,
       ),
-      body: CustomContainer(),
+      body: Body(),
     ),
   ));
 }
 
-class CustomContainer extends StatelessWidget {
-  const CustomContainer({super.key});
+class Body extends StatelessWidget {
+  const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 300,
-        height: 300,
-        padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
-        decoration: BoxDecoration(
-          color: Color(0xFFFFE082), //Colors.amber.shade200,
-          border: Border.all(
-            color: Colors.red,
-            width: 5,
-            style: BorderStyle.solid,
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
           ),
-          borderRadius: BorderRadius.circular(45),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.black.withOpacity(0.3),
-                offset: Offset(6, 6),
-                blurRadius: 10,
-                spreadRadius: 10),
-            BoxShadow(
-                color: Colors.blue.withOpacity(0.3),
-                offset: Offset(-6, -6),
-                blurRadius: 10,
-                spreadRadius: 10),
-          ],
-        ),
-        child: Center(
-            child:
-                Container(color: Colors.green, child: Text('Hello Container'))),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+        ],
       ),
     );
   }
