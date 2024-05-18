f0b974c353620e5f5b383cca6326e0c4e7bbe21b
Jerry <75060346+ryan2414@users.noreply.github.com>
2024-05-19 01:20:43 +0900

    stateful widget , stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Study to Container'),
-        backgroundColor: Colors.blue,
-      ),
-      body: Body(),
-    ),
+  runApp(const MaterialApp(
+    home: HomeWidget(),
   ));
+}
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+        child: Scaffold(
+      body: Body(),
+    ));
+  }
 }
 
 class Body extends StatelessWidget {
@@ -17,37 +23,75 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Align(
-          alignment: Alignment.center,
-          child: Container(
-            width: 300,
-            height: 300,
-            decoration: BoxDecoration(
-                color: Colors.red, borderRadius: BorderRadius.circular(150)),
+    return Column(children: [
+      ExampleStateless(),
+      ExampleStateful(index: 3),
+    ]);
+  }
+}
+
+class ExampleStateless extends StatelessWidget {
+  const ExampleStateless({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        color: Colors.amber,
+      ),
+    );
+  }
+}
+
+class ExampleStateful extends StatefulWidget {
+  final int index;
+
+  const ExampleStateful({required this.index, super.key});
+
+  @override
+  State<ExampleStateful> createState() => _ExampleStatefulState();
+}
+
+class _ExampleStatefulState extends State<ExampleStateful> {
+  late int _index;
+  late TextEditingController textController;
+
+  @override
+  void initState() {
+    super.initState();
+    _index = widget.index;
+    textController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            if (_index == 5) {
+              _index = 0;
+              return;
+            }
+
+            _index++;
+          });
+        },
+        child: Container(
+          color: Colors.blueGrey.withOpacity(_index / 5),
+          child: Center(
+            child: Text('$_index'),
           ),
         ),
-        Align(
-          alignment: Alignment.center,
-          child: Container(
-            width: 280,
-            height: 280,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(140),
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.center,
-          child: Text('Count 0',
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 32,
-              )),
-        ),
-      ],
+      ),
     );
   }
 }
